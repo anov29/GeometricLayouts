05ed849863dc4ad479c2f8109a6fb2e54c6cf645
Andrew Novitskiy <Andrew.Novitskiy@gmail.com>
2021-02-23 00:04:10 -0500

    Geometric controller added

---
+++ CherwellCoding2020/Controllers/GeometricController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CherwellCoding2020.Controllers
+{
+
+    [Route("api/[controller]")]
+    public class GeometricController : Controller
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "geometric controller";
+        }
+    
+        [HttpGet]
+        public string GetTriangle(int x, int y)
+        {
+            return "A1"; 
+        }
+
+
+    }
+}
