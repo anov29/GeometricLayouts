fee67034611b499b38878db2dae902988f548df6
Andrew Novitskiy <Andrew.Novitskiy@gmail.com>
2021-02-24 00:12:59 -0500

    Coordinate controller implementation

--- CherwellCoding2020/Controllers/CoordinateController.cs
+++ CherwellCoding2020/Controllers/CoordinateController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing; 
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,12 +13,42 @@ namespace CherwellCoding2020.Controllers
     {
 
         [HttpGet]
-        public string GetTriangle(int? x, int? y)
+        public LinkedList<Point> GetTriangle(char? row, int? column)
         {
-            if (!y.HasValue || !x.HasValue) return "X and Y cannot be null."; 
-            return "A1"; 
-        }
+            LinkedList<Point> coordinates = new LinkedList<Point>();
 
+            if (!row.HasValue || !column.HasValue) return coordinates;
+
+            int columnInt = column.Value;
+            char rowChar = row.Value; 
+
+            Point point1, point2, point3;
+
+            // upper left
+            point1 = new Point((columnInt / 2) * 10, (char.ToUpper(rowChar) - 64) * 10);
+
+            // bottom right
+            point3 = new Point(((columnInt + 1) / 2) * 10, ((char.ToUpper(rowChar) + 1) - 64) * 10);
+
+            // Odd column number means bottom left triangle 
+            if (column % 2 != 0)
+            {
+                // bottom left point
+                point2 = new Point((columnInt / 2) * 10, ((char.ToUpper(rowChar) + 1) - 64) * 10);
+            }
+            else // else upper right triangle
+            {
+                // upper left point
+                point2 = new Point(((columnInt + 1 ) / 2) * 10, (char.ToUpper(rowChar) - 64) * 10);
+
+            }
+
+            coordinates.AddLast(point1);
+            coordinates.AddLast(point2);
+            coordinates.AddLast(point3);
+
+            return coordinates; 
+        }
 
     }
 }
